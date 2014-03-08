e2087d1ae8ca4826441592ed87121ec32b1c0d9a
Anderson Freitas <anderson.amf@gmail.com>
2014-03-08 00:05:42 -0300

    OpenGL 3.3

--- minimal.cpp
+++ minimal.cpp
@@ -26,7 +26,7 @@ int main(void) {
     exit(EXIT_FAILURE);
 
   glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
-  glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 2);
+  glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
   glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
   glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);
 
